bad4c55fc1f42745999d225fe80a8a06157a8244
ytsvetko <yulia.tsvetkov@gmail.com>
2013-02-23 13:22:08 -0500

    meteor

--- hw2/meteor.py
+++ hw2/meteor.py
@@ -1,16 +1,56 @@
 #!/usr/bin/env python
 import argparse # optparse is deprecated
 from itertools import islice # slicing for iterators
+import collections
  
+def count_chunks(hyp, ref):
+  chunks = 0
+  mapped_unigrams = 0
+
+  hyp_dict = collections.defaultdict(list)
+  for ind, word in enumerate(hyp):
+    hyp_dict[word].append(ind)
+
+  prev_hyp_ind = -2
+  curr_chunk = []
+  for r in ref:
+    if  r in hyp_dict:
+      if len(curr_chunk) > 0:
+        if prev_hyp_ind+1 in hyp_dict[r]:
+          curr_chunk.append(r)
+          prev_hyp_ind += 1
+        else:
+          chunks += 1
+          mapped_unigrams += len(curr_chunk)
+          curr_chunk = [r]
+          prev_hyp_ind = hyp_dict[r][0]
+      else:
+        curr_chunk = [r]
+        prev_hyp_ind = hyp_dict[r][0]
+    elif len(curr_chunk)>0 :
+      chunks += 1
+      mapped_unigrams += len(curr_chunk)
+      prev_hyp_ind = -2
+      curr_chunk = []    
+  return  chunks, mapped_unigrams     
+   
 def evaluate(hyp, ref):
+  chunks, mapped_unigrams = count_chunks(hyp, ref)
+  if mapped_unigrams == 0: 
+    p = 1
+  else: 
+    p = 0.6 * (1.0*chunks/mapped_unigrams)**0.2 # weights from multieval
+  return f_mean(hyp, ref) * (1 - p)
+
+def f_mean(hyp, ref):
   h_set = set(hyp)
   r_set = set(ref)
   match = h_set.intersection(r_set)
-  precision =  len(match)/float(len(hyp))
+  precision =  len(match)/float(len(hyp)) 
   recall =  len(match)/float(len(ref))
   if precision+recall == 0 :
     return 0
-  return precision*recall/(0.9*precision+recall)
+  return precision*recall/(0.85*precision+0.25*recall)# weights from multieval
 
 def main():
     parser = argparse.ArgumentParser(description='Evaluate translation hypotheses.')
